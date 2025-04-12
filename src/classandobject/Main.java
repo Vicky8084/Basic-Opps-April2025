14b72300d65a2cbd7f6d05abcf27d9fbf1019364
Vicky Gupta <mrvickygupta1111@gmail.com>
2025-04-13 01:06:06 +0530

    Assigned Details of First Student

--- src/classandobject/Main.java
+++ src/classandobject/Main.java
@@ -5,5 +5,16 @@ public class Main {
         Student student = new Student();
         student.name="vicky";
         System.out.println("Before assigning the value of student :- "+student);
+
+        student.id=123;
+        student.name="Vicky Gupta";
+        student.branch="Computer Science";
+        student.course="MCA";
+        student.dob="03/05/2003";
+        student.email="mrvg4545@gmail.com";
+
+
+        System.out.println("After assigning the value of student :- "+student);
+
     }
 }
