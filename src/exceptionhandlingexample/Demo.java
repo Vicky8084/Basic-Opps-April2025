c8ce913a8c58fc6574ec4fdbf8ac5b566748bd08
Vicky Gupta <mrvickygupta1111@gmail.com>
2025-04-15 02:32:28 +0530

    DivideByZeroException Example Added

---
+++ src/exceptionhandlingexample/Demo.java
@@ -0,0 +1,11 @@
+package exceptionhandlingexample;
+
+public class Demo {
+    public static void main(String[] args) {
+        int a=10,b=0,rslt=0;
+        rslt=a/b;
+        System.out.println("value of a is :- "+a);
+        System.out.println("value of b is :- "+b);
+        System.out.println("value of rslt is :- "+rslt);
+    }
+}
---
+++ src/exceptionhandlingexample/DivideByZeroExceptionExample.java
@@ -0,0 +1,16 @@
+package exceptionhandlingexample;
+
+public class DivideByZeroExceptionExample {
+    public static void main(String[] args) {
+        int a=10,b=0,rslt=0;
+        try{
+            rslt=a/b;
+        }
+        catch (Exception e){
+            System.out.println("Exception Occurred :- "+e.getMessage());
+        }
+        System.out.println("value of a is :- "+a);
+        System.out.println("value of b is :- "+b);
+        System.out.println("value of rslt is :- "+rslt);
+    }
+}
